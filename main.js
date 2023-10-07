7a6d08288c98c4068874e4fd9750d2f10c5beffc
samirfaritov <samirfaritov6@gmail.com>
2023-10-07 14:39:58 +0500

    js9

---
+++ main.js
@@ -0,0 +1,93 @@
+// let name = "Samir"
+// let number = "15"
+// let Shahar = "Samarqand"
+
+// let a = prompt("Ismimni toping")
+
+// if (name === a.trim()) {
+//     confirm("Tog'ri")
+// let b = prompt("Yoshimni toping")
+
+
+// if (number === b.trim()) {
+//     confirm("Tog'ri")
+//     let c = prompt("Qayerda tug'ilgan man")
+
+//     if (Shahar === c.trim()) {
+//     confirm("Welcome")
+//     } else {
+//     alert("Uyinga bor");
+
+//     }
+// } else {
+//     alert("Uyinga bor");
+// }
+
+// } else {
+//     alert("Uyinga bor");
+// }
+
+
+    // let a = 10
+    // let b = 11
+
+    // if (a < b) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+
+    // let a = 25
+    // let b = 50
+    // let c = 100
+
+    // if (a, b > c) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+
+    // let a = 10
+    // let b = 20
+    // let c = 30
+
+    // if(a + b === c) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+
+    // let a = 60
+    // let b = 30
+    // let c = 30
+
+    // if(a - b === c) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+
+
+    // let a = 60
+    // let b = 30
+    // let c = 2
+
+    // if(a / b === c) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+
+
+    // let a = 6
+    // let b = 3
+    // let c = 18
+
+    // if(a * b === c) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+
+
+    
